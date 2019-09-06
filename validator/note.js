9c89889f5f3e6f28ed904440cf9b7b1d60261b92
Berat Onur Ersen <onurersen@gmail.com>
2019-09-06 16:36:22 +0300

    delete functionality implemented

--- validator/note.js
+++ validator/note.js
@@ -1,9 +1,7 @@
 const Joi = require('@hapi/joi');
 
-// note validation
-const noteValidation = (data) => {
+const noteCreateValidation = (data) => {
     const schema = {
-        _id: Joi.string(),
         topic: Joi.string().min(2).required(),
         description: Joi.string().min(2).required(),
         tags: Joi.array().items(Joi.string())
@@ -11,4 +9,23 @@ const noteValidation = (data) => {
     return Joi.validate(data, schema);
 };
 
-module.exports.noteValidation = noteValidation;
+const noteUpdateValidation = (data) => {
+    const schema = {
+        _id: Joi.string().required(),
+        topic: Joi.string().min(2).required(),
+        description: Joi.string().min(2).required(),
+        tags: Joi.array().items(Joi.string())
+    };
+    return Joi.validate(data, schema);
+};
+
+const noteDeleteValidation = (data) => {
+    const schema = {
+        _id: Joi.string().required()
+    };
+    return Joi.validate(data, schema);
+};
+
+module.exports.noteCreateValidation = noteCreateValidation;
+module.exports.noteUpdateValidation = noteUpdateValidation;
+module.exports.noteDeleteValidation = noteDeleteValidation;
